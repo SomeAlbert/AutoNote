727a5bbbb4f3287c57f5f56b8fe9081d613d4adc
xenon <xenon.a@ya.ru>
2020-07-16 19:38:16 +0300

    english model is working

--- app/src/main/java/com/example/autoconspect/MainActivity.kt
+++ app/src/main/java/com/example/autoconspect/MainActivity.kt
@@ -1,88 +1,97 @@
 package com.example.autoconspect
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.SpeechRecognizer
-import android.speech.SpeechRecognizer.createSpeechRecognizer
-import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import android.content.Context
-import android.webkit.PermissionRequest
-import kotlinx.android.synthetic.main.activity_main.*
-import java.security.Permission
 import android.Manifest
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.speech.RecognizerIntent
+import android.os.AsyncTask
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.kaldi.Assets
+import org.kaldi.Model
+import org.kaldi.RecognitionListener
+import org.kaldi.SpeechRecognizer
+import java.io.File
+import java.io.IOException
+import java.lang.ref.WeakReference
 
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var sr: SpeechRecognizer
-    private val permissionRequestCode = 102
-    private val permissions = arrayOf(
-        Manifest.permission.INTERNET,
-        Manifest.permission.RECORD_AUDIO
-    )
-    val TAG = "STT"
+class MainActivity : AppCompatActivity(), RecognitionListener {
+    companion object {
+        const val TAG = "STT"
+        const val permissionRequestCode = 102
+        const val STATE_START = 0
+        const val STATE_READY = 1
+        const val STATE_DONE = 2
+        const val STATE_MIC = 3
+    }
+
+    init {
+        System.loadLibrary("kaldi_jni")
+    }
+
+    private var sr: SpeechRecognizer? = null
+    private lateinit var model: Model
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ActivityCompat.requestPermissions(this, permissions, permissionRequestCode)
-
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ru-RU")
+        setUiState(STATE_START)
         start_listener.setOnClickListener {
-            sr = createSpeechRecognizer(this)
-            sr.setRecognitionListener(RecListener())
-            sr.startListening(intent)
+            recognizeMicro()
+        }
+
+        val permissionCheck =
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO)
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                permissionRequestCode
+            )
+        }
+
+        SetupTask(this).execute()
+    }
+
+    private class SetupTask(activity: MainActivity) : AsyncTask<Void, Void, Exception>() {
+        var activityReference: WeakReference<MainActivity>? = null
+
+        init {
+            this.activityReference = WeakReference<MainActivity>(activity)
+        }
+
+        override fun doInBackground(vararg params: Void?): Exception? {
+            try {
+                val assets = Assets(activityReference?.get())
+                val assetDir: File = assets.syncAssets()
+                Log.d(TAG, "Sync files in folder: $assetDir")
+
+                // Vosk.SetLogLevel(0)
+
+                activityReference?.get()?.model = Model("$assetDir/model-android") //fixme model
+            } catch (e: IOException) {
+                return e
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: Exception?) {
+            if (result != null) {
+                activityReference?.get()
+                    ?.setErrorState("Failed to initialize the recognizer $result")
+            } else run {
+                activityReference?.get()?.setUiState(STATE_READY)
+            }
         }
     }
 
-        class RecListener : RecognitionListener {
-            override fun onReadyForSpeech(params: Bundle?) {
-                Log.d("STT", "ReadyForSpeech")
-            }
 
-            override fun onRmsChanged(rmsdB: Float) {}
-
-            override fun onBeginningOfSpeech() {}
-
-            override fun onEndOfSpeech() {}
-
-            override fun onBufferReceived(buffer: ByteArray?) {}
-
-            override fun onError(error: Int) {
-                val errorText: String = when (error) {
-                    SpeechRecognizer.ERROR_AUDIO -> "Error Audio"
-                    SpeechRecognizer.ERROR_CLIENT -> " Error client"
-                    SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Permission error"
-                    SpeechRecognizer.ERROR_NETWORK -> "Networl error"
-                    else -> "unknown error" //todo finish this
-                }
-                Log.e("STT", "Error: $errorText")
-            }
-
-            override fun onPartialResults(partialResults: Bundle?) {}
-
-            override fun onEvent(eventType: Int, params: Bundle?) {}
-
-            override fun onResults(results: Bundle?) {
-                val matches: ArrayList<String> =
-                    results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
-                var res : String = ""
-                for (i in 1..matches.size) {
-                    res += i.toString()
-                }
-                Log.d("STTRES", res)
-            }
-    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -91,15 +100,88 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             permissionRequestCode -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    //use it
+                    SetupTask(this).execute()
                 } else {
-                    //oops
+                    finish()
                 }
             }
-            else -> {//wrong id
+            else -> {
+                Log.d("STT", "wrong permission response code")
+                finish()
             }
         }
 
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sr?.cancel()
+        sr?.shutdown()
+    }
+
+    override fun onResult(p0: String?) {
+        speechView.append("$p0\n")
+    }
+
+    override fun onPartialResult(p0: String?) {}
+
+    override fun onError(p0: java.lang.Exception?) {
+        p0?.message?.let { setErrorState(it) }
+    }
+
+    override fun onTimeout() {
+        sr?.cancel()
+        sr = null
+        setUiState(STATE_READY)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setUiState(state: Int) {
+        when (state) {
+            STATE_START -> {
+                infoView.text = "Preparing the recognizer"
+                speechView.movementMethod = ScrollingMovementMethod()
+                start_listener.isEnabled = false
+            }
+            STATE_READY -> {
+                infoView.text = "ready"
+                start_listener.text = "Recognize micro"
+                start_listener.isEnabled = true
+            }
+            STATE_DONE -> {
+                start_listener.text = "recognize micro"
+                start_listener.isEnabled = true
+            }
+            STATE_MIC -> {
+                start_listener.text = "stop micro"
+                infoView.text = "Say something"
+                start_listener.isEnabled = true
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setErrorState(message: String) {
+        infoView.text = message
+        start_listener.text = "recognize micro"
+        start_listener.isEnabled = false
+    }
+
+    fun recognizeMicro() {
+        if (sr != null) {
+            setUiState(STATE_DONE)
+            sr?.cancel()
+            sr = null
+        } else {
+            setUiState(STATE_MIC)
+            try {
+                sr = SpeechRecognizer(model)
+                sr?.addListener(this)
+                sr?.startListening()
+            } catch (e: IOException) {
+                e.message?.let { setErrorState(it) }
+            }
+        }
+    }
+
 }
-// is it working
