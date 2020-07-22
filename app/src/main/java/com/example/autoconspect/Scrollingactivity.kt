a14c47d5dada0995a145ef9e916824fece73a319
SomeAlbert <44546646+SomeAlbert@users.noreply.github.com>
2020-07-22 20:30:15 +0300

    changelist: -added english png for button -edited text viewer (sraactivity) -added down arrow to the bottom of MainActivity

--- app/src/main/java/com/example/autoconspect/Scrollingactivity.kt
+++ app/src/main/java/com/example/autoconspect/Scrollingactivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
-import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.scrollingactivity.*
 
@@ -12,21 +11,15 @@ class Scrollingactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scrollingactivity)
-        russian.setOnClickListener {
+        eng.setOnClickListener {
             val animation = AnimationUtils.loadAnimation(this, R.anim.elevate)
-            imageView.startAnimation(animation)
+            engView.startAnimation(animation)
             val intent = Intent(this, LessonbaseActivity::class.java) //база кнспектов
             startActivity(intent)
         }
-        english2.setOnClickListener {
-            val animation = AnimationUtils.loadAnimation(this, R.anim.etransparency)
-            english2.startAnimation(animation)
-            val intent = Intent(this, LessonbaseActivity::class.java) //база кнспектов
-            startActivity(intent)
-        }
-        lesson3.setOnClickListener {
-            val animation = AnimationUtils.loadAnimation(this, R.anim.etransparency)
-            lesson3.startAnimation(animation)
+        eng.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.elevate)
+            engView.startAnimation(animation)
             val intent = Intent(this, LessonbaseActivity::class.java) //база кнспектов
             startActivity(intent)
         }
